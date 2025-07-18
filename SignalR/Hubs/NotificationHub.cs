c092b7a6af5415bb0e0bde2c93de655b775f461d
Alexei Rotari <alex21.it.dd@gmail.com>
2025-07-18 12:24:37 +0200

    add signalR project

---
+++ SignalR/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public Task SendMessage(string message)
+        {
+            return Clients.Others.SendAsync("Send", message);
+        }
+    }
+}
---
+++ SignalRClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalRClient;
+
+
+
+internal class Program
+{
+    static HubConnection HubConnection;
+
+    static async Task Main(string[] args)
+    {
+        HubConnection = new HubConnectionBuilder().WithUrl("https://localhost:7188/notification").Build();
+
+        HubConnection.On<string>("Send", message => Console.WriteLine($"Message from server: {message}"));
+
+        await HubConnection.StartAsync();
+
+        bool isExit = false;
+
+        while (!isExit)
+        {
+            var message = Console.ReadLine();
+
+            if (message != "isExit")
+            {
+                await HubConnection.SendAsync("SendMessage", message);
+            }
+            else
+            {
+                isExit = true;
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
